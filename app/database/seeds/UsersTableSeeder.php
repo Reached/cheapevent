13cc2df2a84e8c44876e22fb206172ca1309788c
Reached <casper.aarby.sorensen@gmail.com>
2014-06-18 02:08:01 +0300

    admin user

--- app/database/seeds/UsersTableSeeder.php
+++ app/database/seeds/UsersTableSeeder.php
@@ -3,10 +3,10 @@
 class UsersTableSeeder extends Seeder {
 	public function run() {
 		$user = new User;
-		$user->firstname = 'Casper';
-		$user->lastname = 'Sørensen';
-		$user->email = 'casper.aarby.sorensen@gmail.com';
-		$user->password = Hash::make('Caspers88');
+		$user->firstname = 'admin';
+		$user->lastname = 'admin';
+		$user->email = 'info@cheapevent.com';
+		$user->password = Hash::make('g5QqTjqNc4sj9tIR2zZW');
 		$user->admin = 0;
 		$user->save();
 	}
