bd7b0f2a14dd867c6a8d9dc7bc4d3f0010052179
Reached <casper.aarby.sorensen@gmail.com>
2014-07-09 22:39:24 +0300

    teeeest

--- app/views/layouts/main.blade.php
+++ app/views/layouts/main.blade.php
@@ -20,6 +20,7 @@
       <script src="js/html5shiv.js"></script>
       <script src="js/respond.min.js"></script>
       <![endif]-->
+      {{ HTML::script('js/modernizr.custom.js') }}
     </head>
     <body>
 
@@ -42,7 +43,7 @@
       <section class="intro-section" id="home">
         <div class="header-bg">
           <div class="container">
-            <a href="/" class="logo"><span style="font-weight: bold;">Cheap</span><span style="font-weight: 100;">Event</span></a>
+            <a href="/" class="logo"><span style="font-weight: bold;">Cheapszzzz</span><span style="font-weight: 100;">Event</span></a>
             <a id="trigger-overlay" class="menu-btn"><i class="fa fa-bars"></i></a>
             <div class="overlay overlay-hugeinc">
               <div class="container">
